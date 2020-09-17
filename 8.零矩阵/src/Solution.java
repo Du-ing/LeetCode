75c0eb5722733fc218d0b28b7c92ca68f12225a8
D-jun <2385790938@qq.com>
2020-09-17 13:54:36 +0800

    day7

---
+++ 8.零矩阵/src/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * https://leetcode-cn.com/problems/zero-matrix-lcci/
+ */
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (row[i] == 1){
+                for (int j1 = 0; j1 < n; j1++) {
+                    matrix[i][j1] = 0;
+                }
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (col[j] == 1){
+                for (int i1 = 0; i1 < m; i1++) {
+                    matrix[i1][j] = 0;
+                }
+            }
+        }
+    }
+}
---
+++ 9.字符串轮转/src/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isFlipedString(String s1, String s2) {
+        if (s1.length() != s2.length()){
+            return false;
+        }
+        String S = s2 + s2;
+        return S.contains(s1);
+    }
+}
