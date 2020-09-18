d8861b2255208b1aacd7cdb59828799c829cebef
D-jun <2385790938@qq.com>
2020-09-18 14:33:05 +0800

    day8

---
+++ 11.返回倒数第 k 个节点/src/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * https://leetcode-cn.com/problems/kth-node-from-end-of-list-lcci/
+ */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    public int kthToLast(ListNode head, int k) {
+        //统计节点数法
+//        ListNode p = head;
+//        int num = 1;
+//        while (p.next != null){
+//            p = p.next;
+//            num++;
+//        }
+//        int flag = num - k;
+//        ListNode q = head;
+//        for (int i = 0; i < flag; i++) {
+//            q = q.next;
+//        }
+//        return q.val;
+
+        //双指针法，快慢指针
+        if (head.next == null){
+            return head.val;
+        }
+        ListNode front = head;
+        ListNode behind = head;
+        for (int i = 0;; i++) {
+            front = front.next;
+            if (i >= k-1){
+                behind = behind.next;
+            }
+            if (front.next == null){
+                return behind.val;
+            }
+        }
+    }
+}
