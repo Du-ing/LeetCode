1c32d25e63538cad996b89372da7752166d64565
D-jun <2385790938@qq.com>
2020-09-23 20:02:43 +0800

    day12

---
+++ 21.化栈为队/src/MyQueue.java
@@ -0,0 +1,67 @@
+/**
+ * https://leetcode-cn.com/problems/implement-queue-using-stacks-lcci/
+ */
+
+import java.util.Stack;
+
+class MyQueue {
+
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        int n = stack2.size();
+        if (n != 0){
+            for (int i = 0; i < n; i++) {
+                stack1.push(stack2.pop());
+            }
+        }
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        int n = stack1.size();
+        if (n != 0){
+            for (int i = 0; i < n; i++) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        int n = stack1.size();
+        if (n != 0){
+            for (int i = 0; i < n; i++) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        if (stack1.isEmpty() && stack2.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
