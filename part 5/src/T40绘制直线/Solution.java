648609a97f8fda5ae8fe233c4f74cc2716746b50
D-jun <2385790938@qq.com>
2020-10-09 22:34:48 +0800

    day27

--- part 5/src/T40绘制直线/Solution.java
+++ part 5/src/T40绘制直线/Solution.java
@@ -1,25 +1,28 @@
 package T40绘制直线;
 
+import com.sun.org.apache.xerces.internal.impl.dv.xs.IntegerDV;
+
 /**
  *https://leetcode-cn.com/problems/draw-line-lcci/
  */
 
 class Solution {
     public int[] drawLine(int length, int w, int x1, int x2, int y) {
-        int[] ret = new int[length];
-        // 注意根据所在行数计算偏移量
-        int offset = y * w / 32;
-        // 首位数字下标
-        int head = x1 / 32 + offset;
-        // 末位数字下标
-        int rear = x2 / 32 + offset;
-        // 把涉及到的数全部置 -1 也就是 0b11111111111111111111111111111111
-        for (int i = head; i <= rear; i++)
-            ret[i] = -1;
-        // 调整首位数字
-        ret[head] = ret[head] & -1 >>> x1 % 32;
-        // 调整末位数字
-        ret[rear] = ret[rear] & Integer.MIN_VALUE >> x2 % 32;
-        return ret;
+        int[] res = new int[length];
+        int offset = y * w / 32;    //偏移量
+        int first = x1 / 32 + offset;
+        int last = x2 / 32 + offset;
+        for(int i = first; i <= last; i++)     res[i] = -1;
+        if (first != last){
+            //处理起始位置
+            res[first] = res[first] >>> (x1 % 32);
+            //处理最后位置
+            res[last] = Integer.MIN_VALUE >> (x2 % 32);
+        }else {
+            int head = res[first] >>> (x1 % 32);
+            int tail = Integer.MIN_VALUE >> (x2 % 32);
+            res[first] = head & tail;
+        }
+        return res;
     }
 }
