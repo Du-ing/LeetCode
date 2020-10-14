3275100f4934ae598761c1c2b63800f637db4e79
D-jun <2385790938@qq.com>
2020-10-14 18:11:10 +0800

    day32

--- part 8/src/T48有重复字符串的排列组合/Solution.java
+++ part 8/src/T48有重复字符串的排列组合/Solution.java
@@ -4,53 +4,36 @@ package T48有重复字符串的排列组合;
  * https://leetcode-cn.com/problems/permutation-ii-lcci/
  */
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
     public String[] permutation(String S) {
         int n = S.length();
-
-        int len = 1;
-        for (int i = 1; i <= n; i++) {
-            len *= i;
-        }
-
         char[] chars = S.toCharArray();
-        String[] res = new String[len];
-        int k = 0;
+        Set<String> res = new HashSet<>();
         StringBuilder sb = new StringBuilder();
-        dfs(chars,k,0,n,sb,res);
-        //截取结果数组，去掉后面所有的null
-        int i = 0;
-        while(true){
-            if(i == res.length || res[i] == null)   break;
-            i++;
-        }
-        return Arrays.copyOf(res,i);
+        dfs(chars,0,n,sb,res);
+        return res.toArray(new String[res.size()]);
     }
 
-    private int dfs(char[] chars,int k,int num,int n,StringBuilder sb,String[] res){
-        int newNum = num;
+    private void dfs(char[] chars, int k, int n, StringBuilder sb, Set<String> res){
         if (k == n){
-            res[num] = sb.toString();
-            newNum += 1;
+            res.add(sb.toString());
         }else {
             //集合存储遍历过的元素避免重复
             Set<Character> set = new HashSet<>();
-            for (int i = 0; i < chars.length; i++) {
+            for (int i = 0; i < n; i++) {
                 char c = chars[i];
                 if (chars[i] != '0' && set.add(c)) {
                     sb.append(c);
                     chars[i] = '0';
-                    newNum = dfs(chars, k + 1, newNum, n, sb, res);
+                    dfs(chars, k + 1, n, sb, res);
                     //回溯
                     sb.deleteCharAt(sb.length() - 1);
                     chars[i] = c;
                 }
             }
         }
-        return newNum;
     }
 }
