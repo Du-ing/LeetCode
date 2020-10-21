94a0216bf65824ee1e007f71867e42fc619e0460
D-jun <2385790938@qq.com>
2020-10-22 00:21:15 +0800

    day37

--- part 10/src/T53合并排序的数组/Solution.java
+++ part 10/src/T53合并排序的数组/Solution.java
@@ -1,29 +1,32 @@
-import java.util.LinkedList;
+package T53合并排序的数组;
+
+/**
+ * https://leetcode-cn.com/problems/sorted-merge-lcci/
+ */
 
 class Solution {
     public void merge(int[] A, int m, int[] B, int n) {
         if(n == 0) return;
-        LinkedList<Integer> queue = new LinkedList<>();
+        int f = 0;
+        int[] flag = new int[m+n];
+
         int k = 0;
         for(int i=0; i<m+n; i++){
             if(i < m){
-                if(B[k] >= A[i]){
-                    queue.offer(A[i]);
+                if(k >= n || B[k] >= A[i]){
+                    flag[f++] = A[i];
                 }else{
-                    queue.offer(B[k]);
-                    k++;
-                    i--;
+                    flag[f++] = B[k++];
+                    i--;//回退
                 }
             }else{
                 if(k < n){
-                    queue.offer(B[k]);
-                    k++;
+                    flag[f++] = B[k++];
                 }
             }
         }
-        for(int j=0;; j++){
-            A[j] = queue.poll();
-            if(queue.isEmpty()) break;
+        for(int j=0; j<m+n; j++){
+            A[j] = flag[j];
         }
     }
 }
