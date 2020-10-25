40eb78ca33e1afde90a3f0133ac390ab95a920af
D-jun <2385790938@qq.com>
2020-10-25 16:13:43 +0800

    day40

--- part 10/src/T56搜索旋转数组/Solution.java
+++ part 10/src/T56搜索旋转数组/Solution.java
@@ -8,17 +8,17 @@ class Solution {
     public int search(int[] arr, int target) {
         int a = 0;
         int b = arr.length - 1;
-        
+
         while(a <= b){
             int mid = (a + b)/2;
-            if(arr[a] < arr[mid]){ //中间<最左，所以左边非升序，右边升序
+            if(arr[a] < arr[mid]){ //中间>最左，所以左边升序，右边非升序
                 if(target >= arr[a] && target <= arr[mid]){
                     b = mid;
                 }else{
                     a = mid + 1;
                 }
             }
-            else if(arr[a] > arr[mid]){ //中间>最左，所以左边升序，右边非升序
+            else if(arr[a] > arr[mid]){ //中间<最左，所以左边非升序，右边升序
                 if(target >= arr[a] || target <= arr[mid]){
                     b = mid;
                 }else{
