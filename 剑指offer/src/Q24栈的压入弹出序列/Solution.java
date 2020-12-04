a76f0ad16572714f62efaee9876c3dceac7b56fb
D-jun <2385790938@qq.com>
2020-12-04 15:00:23 +0800

    day74

--- 剑指offer/src/Q24栈的压入弹出序列/Solution.java
+++ 剑指offer/src/Q24栈的压入弹出序列/Solution.java
@@ -1,7 +1,27 @@
 package Q24栈的压入弹出序列;
 
+import java.util.Stack;
+
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        return true;
+        int len = pushed.length;
+        Stack<Integer> stack = new Stack<>();
+        int k = 0;
+//        for(int i = 0; i < len; i++){
+//            while((stack.isEmpty() || !stack.peek().equals(popped[i])) && k < len){
+//                stack.push(pushed[k++]);
+//            }
+//            if(stack.peek().equals(popped[i])){
+//                stack.pop();
+//            }
+//        }
+        for(int i = 0; i < len; i++){
+            stack.push(pushed[i]);
+            while(!stack.isEmpty() && stack.peek().equals(popped[k])){
+                stack.pop();
+                k++;
+            }
+        }
+        return stack.isEmpty();
     }
 }
