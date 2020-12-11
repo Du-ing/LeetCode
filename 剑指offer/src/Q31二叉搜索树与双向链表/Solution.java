4d18069970cf5fe7866d6d5c42d30a93a8376c25
D-jun <2385790938@qq.com>
2020-12-11 16:09:36 +0800

    day911

---
+++ 剑指offer/src/Q31二叉搜索树与双向链表/Solution.java
@@ -0,0 +1,52 @@
+package Q31二叉搜索树与双向链表;
+
+import java.util.ArrayList;
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+
+class Solution {
+    public Node treeToDoublyList(Node root) {
+        if(root == null) return null;
+        ArrayList<Node> list = new ArrayList<>();
+        sort(list,root);
+        Node head = list.get(0);
+        int i = 0;
+        while(true){
+            Node node = list.get(i);
+            if(i == list.size() - 1){
+                node.right = head;
+                head.left = node;
+                break;
+            }else{
+                Node next = list.get(i + 1);
+                node.right = next;
+                next.left = node;
+            }
+            i++;
+        }
+        return head;
+    }
+
+    void sort(ArrayList<Node> list,Node node){
+        if(node == null) return;
+        sort(list,node.left);
+        list.add(node);
+        sort(list,node.right);
+    }
+}
